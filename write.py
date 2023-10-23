618fadd70c714af9b0f528276dd536b40338d244
MrBased <felipeaguilarwenig@uc.cl>
2023-10-22 23:24:37 -0300

    Initial commit

---
+++ write.py
@@ -0,0 +1,48 @@
+from docx import Document
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import Pt
+
+
+template_path = "TemplateLime.docx"
+new_file_path = "essay_code.docx"
+
+document = Document(template_path)
+document.save(new_file_path)
+
+
+new_document = Document(new_file_path)
+
+
+insert_paragraph_index1 = 8
+insert_paragraph_index2 = 13
+
+
+def insert_text_with_bullet(document, paragraph_index, text):
+    paragraph = document.paragraphs[paragraph_index]
+
+    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+    run = paragraph.add_run("- ")
+    run.font.name = "Arial"
+    run.font.size = Pt(11)
+    run.add_text(" ")
+    run.add_text(text)
+    paragraph.add_run("\n")
+
+
+n = 0
+n_good_comments = int(input("How many good comments do you want to insert? "))
+while n < n_good_comments:
+    insert_text_with_bullet(new_document, insert_paragraph_index1, input("Your comment:"))
+    n += 1
+
+
+n = 0
+n_bad_comments = int(input("How many bad comments do you want to insert? "))
+while n < n_bad_comments:
+    insert_text_with_bullet(new_document, insert_paragraph_index2, input("Your comment:"))
+    n += 1
+
+new_document.save(new_file_path)
+
+print("Text with consistent bullet points and newline for the next bullet point has been inserted into the document.")
