329428ebed672d80c0e7ceab66fe2e6d9b5d1b27
MrBased <felipeaguilarwenig@uc.cl>
2023-10-23 11:57:10 -0300

    OO refactor

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, send_file
-from docx import Document
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.shared import Pt
+from write import DocWriter
 import os
 
 
@@ -9,6 +7,7 @@ insert_paragraph_index1 = 8
 insert_paragraph_index2 = 13
 
 app = Flask(__name__)
+writer = DocWriter()
 
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -18,40 +17,23 @@ if not os.path.exists(uploads_dir):
     os.makedirs(uploads_dir)
 
 
-def insert_text_with_bullet(document, paragraph_index, text):
-    paragraph = document.paragraphs[paragraph_index]
-
-    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-
-    run = paragraph.add_run("- ")
-    run.font.name = "Arial"
-    run.font.size = Pt(11)
-    run.add_text(" ")
-    run.add_text(text)
-    paragraph.add_run("\n")
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         n_good_comments = int(request.form['n_good_comments'])
         n_bad_comments = int(request.form['n_bad_comments'])
 
-        new_file_path = "src/static/result.docx"
-        template_path = "src/TemplateLime.docx"
-
-        new_document = Document(template_path)
+        writer.set_document()
 
         for _ in range(n_good_comments):
             comment = request.form.get('good_comment')
-            insert_text_with_bullet(new_document, insert_paragraph_index1, comment)
+            writer.insert_text_with_bullet(insert_paragraph_index1, comment)
 
         for _ in range(n_bad_comments):
             comment = request.form.get('bad_comment')
-            insert_text_with_bullet(new_document, insert_paragraph_index2, comment)
+            writer.insert_text_with_bullet(insert_paragraph_index2, comment)
 
-        new_document.save(new_file_path)
-        # pdf_path = "static/essay_code.pdf"
+        writer.save_document()
 
         return render_template('index.html', pdf_created=True)
     return render_template('index.html', pdf_created=False)
@@ -59,7 +41,7 @@ def index():
 
 @app.route('/download_pdf')
 def download_pdf():
-    pdf_path = "static/essay_code.docx"
+    pdf_path = "static/result.docx"
     return send_file(pdf_path, as_attachment=True)
 
 
--- src/write.py
+++ src/write.py
@@ -1,48 +1,33 @@
 from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Pt
+import os
 
 
-template_path = "TemplateLime.docx"
-new_file_path = "essay_code.docx"
+class DocWriter():
 
-document = Document(template_path)
-document.save(new_file_path)
+    insert_paragraph_index1 = 8
+    insert_paragraph_index2 = 13
 
+    def __init__(self):
+        self.template_path = os.path.join("src", "TemplateLime.docx")
+        self.new_file_path = os.path.join("src", "static", "result.docx")
+        self.new_document = Document(self.template_path)
 
-new_document = Document(new_file_path)
+    def set_document(self):
+        self.new_document = Document(self.template_path)
 
+    def save_document(self):
+        self.new_document.save(self.new_file_path)
 
-insert_paragraph_index1 = 8
-insert_paragraph_index2 = 13
+    def insert_text_with_bullet(self, paragraph_index, text):
+        paragraph = self.new_document.paragraphs[paragraph_index]
 
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
 
-def insert_text_with_bullet(document, paragraph_index, text):
-    paragraph = document.paragraphs[paragraph_index]
-
-    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-
-    run = paragraph.add_run("- ")
-    run.font.name = "Arial"
-    run.font.size = Pt(11)
-    run.add_text(" ")
-    run.add_text(text)
-    paragraph.add_run("\n")
-
-
-n = 0
-n_good_comments = int(input("How many positive comments do you want to add? "))
-while n < n_good_comments:
-    insert_text_with_bullet(new_document, insert_paragraph_index1, input("Your comment: "))
-    n += 1
-
-
-n = 0
-n_bad_comments = int(input("How many suggestions for improvement do you want to add? "))
-while n < n_bad_comments:
-    insert_text_with_bullet(new_document, insert_paragraph_index2, input("Your comment: "))
-    n += 1
-
-new_document.save(new_file_path)
-
-print("File saved!")
+        run = paragraph.add_run("- ")
+        run.font.name = "Arial"
+        run.font.size = Pt(11)
+        run.add_text(" ")
+        run.add_text(text)
+        paragraph.add_run("\n")
