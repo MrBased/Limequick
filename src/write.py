1bdd6cd469d8507ac5caadc35f0022232b978630
MrBased <felipeaguilarwenig@uc.cl>
2023-10-23 01:06:13 -0300

    prompts rewrite

--- src/write.py
+++ src/write.py
@@ -33,14 +33,14 @@ def insert_text_with_bullet(document, paragraph_index, text):
 n = 0
 n_good_comments = int(input("How many positive comments do you want to add? "))
 while n < n_good_comments:
-    insert_text_with_bullet(new_document, insert_paragraph_index1, input("Your comment:"))
+    insert_text_with_bullet(new_document, insert_paragraph_index1, input("Your comment: "))
     n += 1
 
 
 n = 0
 n_bad_comments = int(input("How many suggestions for improvement do you want to add? "))
 while n < n_bad_comments:
-    insert_text_with_bullet(new_document, insert_paragraph_index2, input("Your comment:"))
+    insert_text_with_bullet(new_document, insert_paragraph_index2, input("Your comment: "))
     n += 1
 
 new_document.save(new_file_path)
